0f62631af9614794117b930433b4f2968fe2a234
SaneBox <90852065+sanebox-drdev@users.noreply.github.com>
2023-01-30 17:30:32 -0500

    Initial commit

---
+++ app/controllers/oauth_controller.rb
@@ -0,0 +1,10 @@
+class OauthController < ApplicationController
+
+  def index
+    # TODO: Implement first leg of OAuth2 credential exchange here.
+  end
+
+  def callback
+    # TODO: Implement second leg of OAuth2 credential exchange here.
+  end
+end
